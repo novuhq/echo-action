054ce439a803de0ffeff566c5ff3f1464ae9e8ab
Denis Kralj <denis@novu.co>
2024-06-28 14:02:17 +0200

    chore: update readme, update parameters, add validation

--- src/main.ts
+++ src/main.ts
@@ -8,34 +8,40 @@ import axios from 'axios'
 export async function run(): Promise<void> {
   try {
     const bridgeUrl: string = core.getInput('bridge-url')
+    const secretKey: string = core.getInput('secret-key')
+    const apiUrl: string = core.getInput('api-url')
+
+    if (!bridgeUrl) {
+      throw new Error('bridgeUrl must be set')
+    }
+    if (!secretKey) {
+      throw new Error('secretKey must be set')
+    }
+    if (!apiUrl) {
+      throw new Error('apiUrl must be set')
+    }
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
     core.debug(`Bridge URL ${bridgeUrl} ...`)
 
-    const inputs = {
-      novuApiKey: core.getInput('novu-api-key'),
-      bridgeUrl: core.getInput('bridge-url'),
-      apiUrl: core.getInput('api-url')
-    }
-
-    const response = await syncState(
-      inputs.bridgeUrl,
-      inputs.novuApiKey,
-      inputs.apiUrl
-    )
+    const response = await syncState(bridgeUrl, secretKey, apiUrl)
 
     // Set outputs for other workflow steps to use
     core.setOutput('result', response)
     core.setOutput('success', true)
   } catch (error) {
     // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    }
+    core.setOutput('result', {})
+    core.setOutput('success', false)
   }
 }
 
 export async function syncState(
   bridgeUrl: string,
-  novuApiKey: string,
+  secretKey: string,
   apiUrl: string
 ): Promise<object> {
   const sync = await axios.post(
@@ -46,7 +52,7 @@ export async function syncState(
     {
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `ApiKey ${novuApiKey}`
+        Authorization: `ApiKey ${secretKey}`
       }
     }
   )
